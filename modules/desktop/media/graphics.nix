1ae389765d47f241d2c15162ac7d316b6222a2b8
Alexander Brevig <alexanderbrevig@gmail.com@gmail.com>
2022-09-16 09:37:15 +0200

    feat: add kicad

--- modules/desktop/media/graphics.nix
+++ modules/desktop/media/graphics.nix
@@ -1,45 +1,41 @@
-# modules/desktop/media/graphics.nix
-#
-# The hardest part about switching to linux? Sacrificing Adobe. It really is
-# difficult to replace and its open source alternatives don't *quite* cut it,
-# but enough that I can do a fraction of it on Linux. For the rest I have a
-# second computer dedicated to design work (and gaming).
-
 { config, options, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.media.graphics;
-    configDir = config.dotfiles.configDir;
-in {
+let
+  cfg = config.modules.desktop.media.graphics;
+  configDir = config.dotfiles.configDir;
+in
+{
   options.modules.desktop.media.graphics = {
-    enable         = mkBoolOpt true;
-    tools.enable   = mkBoolOpt true;
-    raster.enable  = mkBoolOpt true;
-    vector.enable  = mkBoolOpt true;
-    models.enable  = mkBoolOpt true;
+    enable = mkBoolOpt true;
+    tools.enable = mkBoolOpt true;
+    raster.enable = mkBoolOpt true;
+    vector.enable = mkBoolOpt true;
+    models.enable = mkBoolOpt true;
+    electronics.enable = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs;
       (if cfg.tools.enable then [
-        imagemagick    # for image manipulation from the shell
-      ] else []) ++
+        imagemagick # for image manipulation from the shell
+      ] else [ ]) ++
 
-      # replaces illustrator & indesign
       (if cfg.vector.enable then [
         inkscape
-      ] else []) ++
+      ] else [ ]) ++
 
-      # Replaces photoshop
       (if cfg.raster.enable then [
         gimp
-      ] else []) ++
+      ] else [ ]) ++
 
-      # 3D modelling
       (if cfg.models.enable then [
         blender
-      ] else []);
+      ] else [ ]) ++
 
+      (if cfg.electronics.enable then [
+        kicad
+      ] else [ ]);
   };
-}
+} 
