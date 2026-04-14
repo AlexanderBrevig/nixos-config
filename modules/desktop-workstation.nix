82967133e8e0f89b3eb64ca44c1ab650f0106f73
ab <alexander.brevig@hey.com>
2026-04-14 20:01:06 +0200

    fix: patch obs for nvidia and nvenc

--- modules/desktop-workstation.nix
+++ modules/desktop-workstation.nix
@@ -1,5 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  obs-nvidia = pkgs.symlinkJoin {
+    name = "obs-studio-nvidia";
+    paths = [
+      (pkgs.wrapOBS {
+        plugins = with pkgs.obs-studio-plugins; [
+          obs-vkcapture
+        ];
+      })
+    ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/obs \
+        --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+    '';
+  };
+in
 {
   # Low-latency audio for production work
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
@@ -13,7 +30,7 @@
 
   environment.systemPackages = with pkgs; [
     reaper
-    obs-studio
+    obs-nvidia
     kdePackages.kdenlive
     wf-recorder
     flameshot
